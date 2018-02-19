4fcb92031deda353f4c911cac1a8f2d8b2bb1d08
gvillablanca <g.villalobo.m@gmail.com>
2018-02-19 00:57:09 -0300

    comentarios y arreglos de estilos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,37 @@
+// funcion para relogear la pagina y poner en 0 todo
 $(document).ready(function() {
         $('.reload').click(function() {
             location.reload();
         });
 });
 
+//conecto con la api 
 fetch('https://opentdb.com/api.php?amount=15&category=11&difficulty=medium')
 .then((response)=>{
     return response.json();
   }) .then((data)=>{
-    console.log(data)
+    console.log(data) // imprimo la data
+    //contador de buenas y de preguntas
     let correctAnswers = 0;
     let counter = 0; 
     $('.multiple_option').on('click', function(){
       $('.info').empty(); // ocultar info 
-      $('.other_option').hide();
+      $('.other_option').hide(); // ocultar boton de true or false
+      //obtengo los valores de la data
       let category = data.results[counter].category
       let typeOp = data.results[counter].type
       let difficult = data.results[counter].difficulty
       let question = data.results[counter].question
       let allAnswers = [];
 
+      //los meto en un contenedor
       allAnswers.push(data.results[counter].correct_answer)
       allAnswers.push(data.results[counter].incorrect_answers[0])
       allAnswers.push(data.results[counter].incorrect_answers[1])
       allAnswers.push(data.results[counter].incorrect_answers[2])
       allAnswers.sort();
 
+      //paso la informacion al div vacio del index y paso los datos (categoria, dificultad, pregunta y opciones multiples de respuestas)
       $('.info').append(`<h5 class="inf">${category} </h5> 
         <h5 class="inf"> Difficulty: ${difficult}</h5>
         <h3 class="question">${question}</h3><div>
@@ -35,23 +41,26 @@ fetch('https://opentdb.com/api.php?amount=15&category=11&difficulty=medium')
         <div><button class="options btn-default btn" >${allAnswers[3]}</button></div>`)
 
       $('.options').on('click', function(){ // evento para respuestas
-        if($(this).text() === data.results[counter].correct_answer  && counter !== 15){
+        if($(this).text() === data.results[counter].correct_answer  && counter !== 15){//verifico si la opcion es correcta
           $('.info').empty(); // vaciar div contenedor info
           $('.multiple_option').show(); // vuelvo a mostrar mi boton
           $('.multiple_option').text('Next Question'); // cambio contenido del boton
           $('.info').append(`<h4><i class="far fa-check-square"></i> Correct answer!</h4>`);
 
+          //y aumento la informacion al array de respuestas correctas
           correctAnswers ++; console.log(correctAnswers) //contador de respuestas correctas
         }
+        //  si las respuestas son incorrectas, imprimo el error y la opcion correcta
         if($(this).text() !== data.results[counter].correct_answer  && counter !== 15){
           $('.info').empty();
           $('.multiple_option').show();
-          $('.multiple_option').text('Next Question');
+          $('.multiple_option').text('Next <br> Question');
           $('.info').append(`<h4><i class="fas fa-times"></i> Wrong answer!</h4><h5><i class="far fa-check-square"></i> Correct Answer is:
           ${data.results[counter].correct_answer}</h5>`);
           
         }counter ++; // contador general
 
+        //al finalizar si acaba el quiz me retorna al inicio
         if(counter === 15){
           $('.info').empty();
           $('.multiple_option').hide();
@@ -59,10 +68,10 @@ fetch('https://opentdb.com/api.php?amount=15&category=11&difficulty=medium')
           <h4>Your results</h4><h5>You got ${correctAnswers} out of 5</h5>`)
          
         } 
-      }); //end answers function
+      });
 
-  }) //end multiple_option
-}); //end then data
+  }) 
+}); 
 
 
 
@@ -77,7 +86,7 @@ fetch('https://opentdb.com/api.php?amount=15&difficulty=hard&type=boolean')
     let correctAnswers = 0;
     let counter = 0; 
     $('.other_option').on('click', function(){
-      $('.info').empty(); // ocultar info 
+      $('.info').empty();
       $('.multiple_option').hide();
       let category = data.results[counter].category
       let difficult = data.results[counter].difficulty
@@ -95,14 +104,14 @@ fetch('https://opentdb.com/api.php?amount=15&difficulty=hard&type=boolean')
         <div><button class="options btn-default btn" >${allAnswers[1]}</button></div>
        `);
 
-      $('.options').on('click', function(){ // evento para respuestas
+      $('.options').on('click', function(){ 
         if($(this).text() === data.results[counter].correct_answer  && counter !== 15){
-          $('.info').empty(); // vaciar div contenedor info
-          $('.other_option').show(); // vuelvo a mostrar mi boton
-          $('.other_option').text('Next Question'); // cambio contenido del boton
+          $('.info').empty(); 
+          $('.other_option').show(); 
+          $('.other_option').text('Next <br> Question'); 
           $('.info').append(`<h4 class="correct"><i class="far fa-check-square"></i> Correct answer!</h4>`);
 
-          correctAnswers ++; console.log(correctAnswers) //contador de respuestas correctas
+          correctAnswers ++; console.log(correctAnswers)
         }
         if($(this).text() !== data.results[counter].correct_answer  && counter !== 15){
           $('.info').empty();
@@ -111,7 +120,7 @@ fetch('https://opentdb.com/api.php?amount=15&difficulty=hard&type=boolean')
           $('.info').append(`<h4 class="error"><i class="fas fa-times"></i> Wrong answer!</h4><h5 class="correct-answer"><i class="far fa-check-square"></i> Correct Answer is: 
           ${data.results[counter].correct_answer}</h5>`);
           
-        }counter ++; // contador general
+        }counter ++; 
 
         if(counter === 15){
           $('.info').empty();
@@ -121,7 +130,7 @@ fetch('https://opentdb.com/api.php?amount=15&difficulty=hard&type=boolean')
          
         } 
 
-      }); //end answers function
+      }); 
 
-  }) //end multiple_option
-}); //end then data
+  })
+});
